ffd30c86fb6e0a686a469e7301129593faac1e7a
krok-code <127609706+krok-code@users.noreply.github.com>
2024-02-18 14:01:40 +0200

    Initial commit

---
+++ src/components/Layout/Title.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import { Title } from './Layout.styled';
+
+const GlobalTitle = ({ title }) => {
+  return <Title>{title}</Title>;
+};
+
+GlobalTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default GlobalTitle;
